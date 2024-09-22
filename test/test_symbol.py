45ab905c1e0fcab53bda2c93d754ba4a14cf3947
Jermaine Clarke <jermaine.oclarke@icloud.com>
2024-09-22 12:31:47 -0500

    Removed test_core.py and moved Symbol tests to test_symbol.py.

--- test/test_symbol.py
+++ test/test_symbol.py
@@ -7,4 +7,23 @@ from unittest import TestCase
 from core import Symbol
 
 class TestSymbol(TestCase):
-    pass
+    def setUp(self):
+        self.sym0 = Symbol('a', 'symbol1', ['budget', 'distance'])
+        self.sym1 = Symbol('p', 'person', ['name', 'age', 'nationality'])
+        self.sym2 = Symbol('q')
+
+    def test_regex(self):
+        self.assertEqual(self.sym0.regex, '^a\(\s*(?P<budget>\w+),\s*(?P<distance>\w+)\)')
+        self.assertEqual(self.sym1.regex, '^p\(\s*(?P<name>\w+),\s*(?P<age>\w+),\s*(?P<nationality>\w+)\)')
+        self.assertEqual(self.sym2.regex, '^q')
+
+    def test_attributes(self):
+        self.assertEqual(self.sym0.name, 'symbol1', 'name does not match')
+        self.assertEqual(self.sym0.size, 2, 'parameter count is incorrect.')
+        self.assertEqual(self.sym0.glyph, 'a', 'incorrect glyph')
+        self.assertEqual(self.sym1.name, 'person', 'name does not match')
+        self.assertEqual(self.sym1.size, 3, 'parameter count is incorrect.')
+        self.assertEqual(self.sym1.glyph, 'p', 'incorrect glyph')
+        self.assertEqual(self.sym2.name, 'q', 'name does not match')
+        self.assertEqual(self.sym2.size, 0, 'parameter count is incorrect.')
+        self.assertEqual(self.sym2.glyph, 'q', 'incorrect glyph')
