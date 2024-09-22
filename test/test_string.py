bf9f120fe1481d22d84a87e5e9d03637faa5c05a
jermaine-clarke <jermaine.oclarke@icloud.com>
2024-09-22 09:40:43 -0500

    Testing stub #4

---
+++ test/test_string.py
@@ -0,0 +1,10 @@
+#  ---------------------------------------------------------------------------------------------------------------------
+#  test_string.py
+#  Copyright (c) 2024. Jermaine Clarke
+#  All rights reserved.
+#  ---------------------------------------------------------------------------------------------------------------------
+from unittest import TestCase, skip
+from core import String
+
+class TestString(TestCase):
+    pass
